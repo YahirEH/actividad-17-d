e068f17dfb85b0d29b049bda9b0b4a38b7e3730d
YahirEH <>
2023-03-29 13:59:07 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>act17</title>
+  </head>
+  <body>
+    <?php
+ 
+    echo'<p>Nombre: Yahir Alejandro Diaz :O</p>'
+?>
+   <?php
+     echo '<p>Calle: Pericos</p>'
+?>
+    <?php
+      echo '<p>Colonia: DEl valle</p>'
+?>
+   <?php
+    echo'<p>Municipio: Sokoto</p>'
+?>
+   <?php
+    echo '<p>Telefono: 834787789</p>'
+?>
+   
+  </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,3 @@
+Inicio
+mostrar textos
+Fin
